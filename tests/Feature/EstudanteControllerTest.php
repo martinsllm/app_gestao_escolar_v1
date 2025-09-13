59029314dd0898e6fa1a27197767bad64b68a753
martinsllm <lucaslm2010@gmail.com>
2025-09-12 21:16:54 -0300

    Add field ativo in model Estudante

--- tests/Feature/EstudanteControllerTest.php
+++ tests/Feature/EstudanteControllerTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\Models\Estudante;
 use App\Models\Turma;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class EstudanteControllerTest extends TestCase
