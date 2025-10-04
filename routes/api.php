d3a2262253e1fbc64c085b88c329b00060d90a0c
martinsllm <lucaslm2010@gmail.com>
2025-10-03 21:40:20 -0300

    Add breeze

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,19 @@
 <?php
-
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EstudanteController;
 use App\Http\Controllers\MedidaController;
 use App\Http\Controllers\OcorrenciaController;
-use App\Http\Controllers\RelatorioOcorrenciaController;
-use App\Http\Controllers\RelatorioTurmaController;
 use App\Http\Controllers\TurmaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     Route::apiResource('turmas', TurmaController::class);
     Route::apiResource('estudantes', EstudanteController::class);
     Route::apiResource('ocorrencias', OcorrenciaController::class);
     Route::apiResource('medidas', MedidaController::class);
-
-    Route::get('/export/ocorrencias/{extensao}', [RelatorioOcorrenciaController::class, 'export']);
-    Route::get('/export/turmas/{extensao}/{id}', [RelatorioTurmaController::class, 'export']);
 });
 
