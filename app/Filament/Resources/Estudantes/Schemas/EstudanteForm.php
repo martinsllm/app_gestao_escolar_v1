0ed6699a8ce376109f09507fc47f8eed9b7ffff5
martinsllm <lucaslm2010@gmail.com>
2025-10-21 20:58:30 -0300

    Creating students form

--- app/Filament/Resources/Estudantes/Schemas/EstudanteForm.php
+++ app/Filament/Resources/Estudantes/Schemas/EstudanteForm.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Resources\Estudantes\Schemas;
 
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class EstudanteForm
@@ -10,7 +13,23 @@ class EstudanteForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('matricula')
+                    ->required()
+                    ->unique()
+                    ->rules('numeric'),
+                TextInput::make('nome_completo')
+                    ->required(),
+                DatePicker::make('data_nascimento')
+                    ->required(),
+                TextInput::make('telefone_responsavel')
+                    ->mask('(99) 99999-9999')
+                    ->required(),
+                TextInput::make('email')
+                    ->email()
+                    ->required(),
+                Select::make('turma_id')
+                    ->relationship('turma', 'codigo')
+                    ->required(),
             ]);
     }
 }
